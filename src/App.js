5741849aef7b2f72c9c9981690d52e1aae2e27f1
Vaibhav Matere <vpm00718897@gmail.com>
2023-01-27 17:33:02 +0530

    style : login page UI added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
+import Login from "./pages/login/login";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">CRM Ticket System</h1>
+      <Login />
     </div>
   );
 }
